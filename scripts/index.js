ad0cc12edd04b9fe5464c441d449c1b25714ebc7
simsimler <sslr.mc@gmail.com>
2021-08-09 00:35:44 +0900

    fix: errors

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const infoDiv = document.querySelector("#__next > div > div.iu-is-the-best.min-h-screen.text-black.dark\\:text-gray-100.dark\\:bg-discord-dark.bg-white > div > div > div > div.lg\\:flex.lg\\:flex-row-reverse > div.mb-1.w-full.lg\\:w-1\\/4 > div.grid.gap-4.grid-cols-2.px-4.py-4.text-black.dark\\:text-gray-400.dark\\:bg-discord-black.bg-little-white.rounded-sm")
-    libChild = document.createElement("div")
-    libChild.innerText = "라이브러리"
-    libChild2 = document.createElement("div")
-    const id = document.URL.split("/")[4]
-    fetch(`https://koreanbots.com/api/v2/bots/${id}`)
-        .then(res => res.json())
-            .then(data => {
-                libChild2.innerText = data.data.lib
-                infoDiv.appendChild(libChild)
-                infoDiv.appendChild(libChild2)
-        }
-    )
-})
+const SELECTOR = "#__next > div > div.iu-is-the-best.min-h-screen.text-black.dark\\:text-gray-100.dark\\:bg-discord-dark.bg-white > div > div > div > div.lg\\:flex.lg\\:flex-row-reverse > div.mb-1.w-full.lg\\:w-1\\/4 > div.grid.gap-4.grid-cols-2.px-4.py-4.text-black.dark\\:text-gray-400.dark\\:bg-discord-black.bg-little-white.rounded-sm"
+
+const checkExist = setInterval(function() {
+    if (document.querySelectorAll(SELECTOR).length) {
+        console.log("Found element, starting update element")
+        const infoDiv = document.querySelector(SELECTOR)
+        libChild = document.createElement("div")
+        libChild.innerText = "라이브러리"
+        libChild2 = document.createElement("div")
+        const id = document.URL.split("/")[4]
+        fetch(`https://koreanbots.dev/api/v2/bots/${id}`)
+            .then(res => res.json())
+                .then(data => {
+                    libChild2.innerText = data.data.lib
+                    infoDiv.appendChild(libChild)
+                    infoDiv.appendChild(libChild2)
+            }
+        )
+       clearInterval(checkExist);
+    }
+ }, 100);
